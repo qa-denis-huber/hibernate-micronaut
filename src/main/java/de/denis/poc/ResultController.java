72cf6a3192d0cc7ad953b11632360691e38c6f9b
Denis Huber <denis.huber@qaware.de>
2020-12-08 22:25:15 +0100

    Test search methods for arrays

--- src/main/java/de/denis/poc/ResultController.java
+++ src/main/java/de/denis/poc/ResultController.java
@@ -30,21 +30,43 @@ public class ResultController {
         return null;
     }
 
-    @Get("/{id}")
+    @Get("/{dim}")
     @Transactional(readOnly = true)
-    public ResultSetEntity findById(@QueryValue String id) {
-        return null;
+    public List<ResultSetEntity> findById(@QueryValue String dim) {
+        // Typed Query
+        // Throws NPE
+//        String qlString = "SELECT rse FROM ResultSetEntity AS rse WHERE '" + dim + "' MEMBER OF rse.dimensions";
+//        return entityManager.createQuery(qlString, ResultSetEntity.class).getResultList();
+
+
+        // Criteria Query
+        // Throws IllegalArgumentException: unknown collection expression type [org.hibernate.query.criteria.internal.path.SingularAttributePath]
+//        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+//
+//        CriteriaQuery<ResultSetEntity> query = cb.createQuery(ResultSetEntity.class);
+//        Root<ResultSetEntity> rse = query.from(ResultSetEntity.class);
+//
+//        Expression<Collection<String>> dimensions = rse.get("dimensions");
+//        Predicate blub = cb.isMember("blub", dimensions);
+//
+//        query.select(rse).where(blub);
+//        return entityManager.createQuery(query).getResultList();
+
+        // Native sql works!
+        String sql = "select * from result_set where '" + dim + "' = ANY (dimensions)";
+        return entityManager.createNativeQuery(sql, ResultSetEntity.class).getResultList();
     }
 
     @Post
     @Transactional
     public ResultSetEntity save() {
         Random random = new Random();
-        int numDim = random.nextInt(5) + 1;
+        int numDim = random.nextInt(5);
         List<String> dimensions = new ArrayList<>(numDim);
         for (int i = 0; i < numDim; i++) {
             dimensions.add("dim #" + random.nextInt(100));
         }
+        dimensions.add("blub");
 
         ResultSetEntity resultSetEntity = new ResultSetEntity(UUID.randomUUID(), dimensions);
         entityManager.merge(resultSetEntity);
